6d63548414f8683df8c80fb2f9f948a51a9fad07
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-25 15:06:27 -0300

    Creating Loops and Arrays

---
+++ 03-loops-and-array/sports.ts
@@ -0,0 +1,14 @@
+let sportsOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
+
+for (let i = 0; i < sportsOne.length; i++)
+    console.log(sportsOne[i]);
+
+console.log("-------------------------");
+
+// Let's use the simplied for loop
+for (let tempSport of sportsOne) {
+    if (tempSport == "Cricket")
+        console.log(tempSport + " << My Favorite!");
+    else
+        console.log(tempSport);
+}
